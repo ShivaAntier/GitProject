85fa60951c6ed98c6b6465964325d3eb602417ae
Amandeep <amandeep261524@gmail.com>
2023-02-01 16:19:06 +0530

    Second commit

--- src/test/java/GitPackage/GitClass.java
+++ src/test/java/GitPackage/GitClass.java
@@ -3,5 +3,8 @@ package GitPackage;
 public class GitClass {
     public static void main(String[] args) {
         System.out.println("this is git project!!");
+        System.out.println("Hello second line");
+
+        System.out.println("sadhkajshd");
     }
 }
